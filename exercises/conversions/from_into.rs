351a3454e2ae3abed5a5256147b327bd25abfe1c
billow <billow.fun@gmail.com>
2023-04-13 02:48:01 +0800

    You made it to the Fe-nish line!

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,20 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let xs = s.split(',').collect::<Vec<_>>();
+        if xs.iter().any(|x| x.is_empty()) || xs.len() != 2 {
+            return Person::default();
+        }
+        if let Ok(age) = xs[1].parse::<usize>() {
+            Person {
+                name: xs[0].to_string(),
+                age,
+            }
+        } else {
+            Person::default()
+        }
     }
 }
 
--- exercises/threads/threads2.rs
+++ exercises/threads/threads2.rs
@@ -3,7 +3,7 @@
 // Building on the last exercise, we want all of the threads to complete their work but this time
 // the spawned threads need to be in charge of updating a shared value: JobStatus.jobs_completed
 
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -12,19 +12,20 @@ struct JobStatus {
 }
 
 fn main() {
-    let status = Arc::new(JobStatus { jobs_completed: 0 });
+    let status = Arc::new(Mutex::new(JobStatus { jobs_completed: 0 }));
     let mut handles = vec![];
     for _ in 0..10 {
         let status_shared = Arc::clone(&status);
         let handle = thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
             // TODO: You must take an action before you update a shared value
-            status_shared.jobs_completed += 1;
+            let mut _mutex_guard = status_shared.lock().unwrap();
+            _mutex_guard.jobs_completed += 1;
         });
         handles.push(handle);
     }
     for handle in handles {
         handle.join().unwrap();
-        println!("jobs completed {}", status.jobs_completed);
+        println!("jobs completed {}", status.lock().unwrap().jobs_completed);
     }
 }
